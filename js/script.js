fd1fba19c87ab4d53a0a588bad4e804f179ae95b
Osinloye Samuel Adebayo <psalmueloye@gmail.com>
2020-10-10 11:44:08 +0100

    Add file

--- js/script.js
+++ js/script.js
@@ -13,14 +13,17 @@ const computerPoint = document.querySelector("#computer-score");
 
 const guessBtn = document.querySelector("#guess");
 const nextRoundBtn = document.querySelector("#next-round");
+function advanceRound() {
+  currentRoundNumber += 1;
+}
 
 function generateTarget() {
   return (Math.floor(Math.random() * 10))
 }
 
 // Event Handlers
-guessBtn.addEventListener("click", makeGuess);
-nextRoundBtn.addEventListener("click", advanceRound);
+// guessBtn.addEventListener("click", makeGuess);
+// nextRoundBtn.addEventListener("click", advanceRound);
 
 function makeGuess() {
   if (humanGuess.value > 9) {
@@ -38,24 +41,21 @@ function compareGuesses(human, computer, guess) {
   let computerPlayer = Math.abs(computer - guess);
 
   if (humanPlayer < computerPlayer) {
-    return humanPlayer;
+    return true;
   } else if (humanPlayer == computerPlayer) {
-    return humanPlayer;
+    return true;
   } else {
-    return computerPlayer;
+    return false;
   }
 }
 
 function updateScore(winner) {
-  // increase uodate score or humanscore by one
   if (winner == true) {
-    humanScore += 1
-
+    humanPoint.innerHTML = humanScore++
+    winner = "Human"
   } else {
-    computerScore += 1
+    computerPoint.innerHTML = computerScore++;
+    winner = "Computer"
   }
 }
 
-function advanceRound() {
-  currentRoundNumber += 1;
-}
